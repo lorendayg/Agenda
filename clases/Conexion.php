bb0ae3dd055cbde15524da0771164ff4242b54e6
michelleromero777 <rmichillearumi@gmail.com>
2025-09-22 22:19:50 -0600

    Update Conexion.php

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -3,9 +3,9 @@
         // servidor (localhost), usuario, password, base son los 4 elementos minimos para hacer una conexion 
         public function conectar(){
             $host = "localhost";
-            $usuario = "root";
-            $password = "";
-            $base = "agenda2";
+            $usuario = "backend";
+            $password = "backend2025";
+            $base = "b221190089_agenda";
             $conexion = mysqli_connect(
                 $host, $usuario, $password, $base
             );
